1b23349b2a63c673693f4a697b11f71d18bf3aaf
Alexander Fedulin <alexander.fedulin@gmail.com>
2017-04-05 12:30:48 +0300

    Changed ActionDispatch to ActiveSupport for Reloader

--- lib/rails_development_boost/reloader.rb
+++ lib/rails_development_boost/reloader.rb
@@ -4,23 +4,23 @@ module RailsDevelopmentBoost
       def force_execute!
         @force_execute = true
       end
-      
+
       def updated?
         @force_execute = true if result = super
         result
       end
-      
+
       def execute
         if RailsDevelopmentBoost.reload_routes_on_any_change || @in_execute_if_updated || @force_execute || updated?
           @force_execute = false
           super
         end
       end
-      
+
       def load(file, wrap = false)
         ActiveSupport::Dependencies.loading_routes_file(file) { super }
       end
-      
+
       def execute_if_updated
         old_in_execute_if_updated = @in_execute_if_updated
         @in_execute_if_updated = true
@@ -29,20 +29,20 @@ module RailsDevelopmentBoost
         @in_execute_if_updated = old_in_execute_if_updated
       end
     end
-    
+
     extend self
-    
+
     def hook_in!
       Rails.application.reloaders.unshift(self)
-      ActionDispatch::Reloader.to_prepare(:prepend => true) { RailsDevelopmentBoost::Reloader.execute_if_updated }
+      ActiveSupport::Reloader.to_prepare(:prepend => true) { RailsDevelopmentBoost::Reloader.execute_if_updated }
       patch_routes_reloader! if Rails::VERSION::MAJOR >= 4
     end
-    
+
     def execute
       init unless @inited
       @last_run_result = ActiveSupport::Dependencies.unload_modified_files!
     end
-    
+
     def execute_if_updated
       @last_run_result.nil? ? execute : @last_run_result
     ensure
@@ -50,11 +50,11 @@ module RailsDevelopmentBoost
     end
 
     alias_method :updated?, :execute
-    
+
     def routes_reloader
       Rails.application.respond_to?(:routes_reloader) && Rails.application.routes_reloader
     end
-    
+
     private
     # Rails 4.0+ calls routes_reloader.execute instead of routes_reloader.execute_if_updated because an autoloaded Rails::Engine might be mounted via routes.rb, therefore if any constants
     # are unloaded this triggers the reloading of routes.rb, we would like to avoid that.
@@ -63,7 +63,7 @@ module RailsDevelopmentBoost
         reloader.extend(RoutesReloaderPatch)
       end
     end
-    
+
     def init
       Rails.application.reloaders.delete_if do |reloader|
         if rails_file_checker?(reloader)
@@ -73,7 +73,7 @@ module RailsDevelopmentBoost
       end
       @inited = true
     end
-    
+
     def pacify_rails_file_checker(file_checker)
       file_checker.singleton_class.class_eval do
         def updated?;           false; end
@@ -81,7 +81,7 @@ module RailsDevelopmentBoost
         def execute_if_updated; false; end
       end
     end
-    
+
     def rails_file_checker?(reloader)
       if (dir_glob = reloader.instance_variable_get(:@glob)).kind_of?(String)
         autoload_paths = ActiveSupport::Dependencies.autoload_paths
